937451cfa240de8a36589a65e39b78df94e7e599
yogi <yogi@yogis-MacBook-Air.local>
2018-07-14 01:15:24 +0800

    COMPLETE: SSM configurations and tests

---
+++ src/main/java/com/hyz/o2o/dao/AreaDao.java
@@ -0,0 +1,9 @@
+package com.hyz.o2o.dao;
+
+import java.util.List;
+
+import com.hyz.o2o.entity.Area;
+
+public interface AreaDao {
+ List<Area> queryArea();
+}
---
+++ src/test/java/com/hyz/o2o/service/AreaServiceTest.java
@@ -0,0 +1,22 @@
+package com.hyz.o2o.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hyz.o2o.BaseTest;
+import com.hyz.o2o.entity.Area;
+
+public class AreaServiceTest extends BaseTest {
+	@Autowired
+	private AreaService areaService;
+
+	@Test
+	public void testGetAreaList() {
+		List<Area> areaList = areaService.getAreaList();
+		assertEquals("西苑", areaList.get(0).getAreaName());
+	}
+}
